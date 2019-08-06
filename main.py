2cca3df2e151687392ba2e665575d8ef6e118731
Andy Dearing <andy.dearing@gmail.com>
2019-08-06 16:25:13 -0400

    working tile set

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import sys
 from PIL import Image
 
-images = map(Image.open, ['Test1.jpg', 'Test2.jpg', 'Test3.jpg'])
-widths, heights = zip(*(i.size for i in images))
+# write in images to be concatinated
+images = ['Walk_000 copy.png', 'Walk_001 copy.png']
 
-total_width = sum(widths)
-max_height = max(heights)
+# hard code image width and height
+width = 1610
+height = 1446
 
-new_im = Image.new('RGB', (total_width, max_height))
+new_image = Image.new('RGBA', (width * len(list(images)), height))
 
-x_offset = 0
-for im in images:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
+corner = 0
 
-new_im.save('test.jpg')
+for image in images:
+    sub_image = Image.open(image)
+    new_image.paste(sub_image.copy(),(corner, 0))
+    corner += width
+
+new_image.show()
+new_image.save('result.png')
+# new_image.show()
+
+# widths, heights = zip(*(i.size for i in images))
+
+# total_width = sum(widths)
+# max_height = max(heights)
+
+# new_im = Image.new('RGB', (total_width, max_height))
+
+# x_offset = 0
+# for im in images:
+#   new_im.paste(im, (x_offset,0))
+#   x_offset += im.size[0]
+
+# new_im.save('test.jpg')
